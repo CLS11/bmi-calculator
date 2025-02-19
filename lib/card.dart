486b247a040b2e1a2956225f2f0de60178b52cd6
crystall.101600 <crystall.101600@gmail.com>
2025-02-19 17:48:09 +0000

    UI for the card.

--- lib/card.dart
+++ lib/card.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
 class MyCard extends StatelessWidget {
-  const MyCard({required this.colour, required this.cardChild, super.key});
+  const MyCard({
+    required this.colour, 
+    required this.cardChild, 
+    required this.onTap, 
+    super.key,
+    });
 
   final Color colour;
   final Widget cardChild;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: colour,
+      body: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: colour,
+          ),
+          child: cardChild,
         ),
-        child: cardChild,
       ),
     );
   }
